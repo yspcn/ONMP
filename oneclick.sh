13b78f12e99312b3944f70b73d9ab1e2cc16c4fb
yspcn <55094985+yspcn@users.noreply.github.com>
2020-03-27 23:09:28 +0800

    Update oneclick.sh

--- oneclick.sh
+++ oneclick.sh
@@ -37,7 +37,7 @@ Install()
 	mkdir -p /opt/onmp
 
     # 获取onmp脚本
-    curl -kfsSL https://raw.githubusercontent.com/xzhih/ONMP/master/onmp.sh > /opt/onmp/onmp.sh
+    curl -kfsSL https://raw.githubusercontent.com/yspcn/ONMP/master/onmp.sh > /opt/onmp/onmp.sh
     # curl -kfsSL http://192.168.4.126:4000/onmp.sh > /opt/onmp/onmp.sh
     chmod +x /opt/onmp/onmp.sh
 
@@ -50,7 +50,7 @@ Install()
 Updata()
 {
 	rm -rf /opt/onmp/onmp.sh
-	curl -kfsSL https://raw.githubusercontent.com/xzhih/ONMP/master/onmp.sh > /opt/onmp/onmp.sh
+	curl -kfsSL https://raw.githubusercontent.com/yspcn/ONMP/master/onmp.sh > /opt/onmp/onmp.sh
 	# curl -kfsSL http://192.168.4.126:4000/onmp.sh > /opt/onmp/onmp.sh
 	chmod +x /opt/onmp/onmp.sh
 	/opt/onmp/onmp.sh renewsh > /dev/null 2>&1
